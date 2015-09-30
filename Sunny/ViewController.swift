f87a56794effabbbab7060d6b3197ef77d0cc5cc
Jaken Herman <jch053@shsu.edu>
2015-09-30 13:21:16 -0500

    worked on main storyboard and added functionality for rain change and humidity. Current temperature shows up wonky, and the data is still loading from a plist, not an api. Lots to do

--- Sunny/ViewController.swift
+++ Sunny/ViewController.swift
@@ -10,15 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var currentTemperatureLabel: UILabel?
+    @IBOutlet weak var currentHumidityLabel: UILabel?
+    @IBOutlet weak var currentPrecipitationLabel: UILabel?
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         if let plistPath = NSBundle.mainBundle().pathForResource("CurrentWeather", ofType: "plist"),
            let WeatherDictionary = NSDictionary(contentsOfFile: plistPath),
-           let currentWeatherDictionary = weatherDictionary["currently"] as? [String: AnyObject]{
+           let currentWeatherDictionary = WeatherDictionary["currently"] as? [String: AnyObject]{
             
             let currentWeather = CurrentWeather(weatherDictionary: currentWeatherDictionary)
+            
+            
+            currentTemperatureLabel?.text = "\(currentWeather.temperature)º"
+            
+            currentHumidityLabel?.text = "\(currentWeather.humidity)%"
+            
+            currentPrecipitationLabel?.text = "\(currentWeather.precipProbability)%"
             
                    }
     }
